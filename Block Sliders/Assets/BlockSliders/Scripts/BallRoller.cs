4707de3bcd87b44f0a015d36532213570e8d6b7f
kaszman <user@ChuckNorrisFanClub.local>
2015-02-13 18:03:30 -0700

    2 block

---
+++ Block Sliders/Assets/BlockSliders/Scripts/BallRoller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallRoller : MonoBehaviour {
+
+	public float speed;
+	private Vector3 acceleration;
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		acceleration = Vector3.zero;
+		
+		//up
+		if (Input.GetKey("w"))
+		{
+			acceleration.x += 1 * speed;
+		}
+		
+		//down
+		if (Input.GetKey("s"))
+		{
+			acceleration.x -= 1 * speed;
+		}
+		
+		//left
+		if (Input.GetKey("a"))
+		{
+			acceleration.z += 1 * speed;
+		}
+
+		//right
+		if (Input.GetKey("d"))
+		{
+			acceleration.z -= 1 * speed;
+		}
+		
+		//acceleration.Normalize();
+		acceleration *= speed;
+		rigidbody.AddTorque(acceleration);
+	}
+}
