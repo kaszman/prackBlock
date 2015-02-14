c1af77814d185d77300668c9269ce8dac4ea716d
kaszman <user@ChuckNorrisFanClub.local>
2015-02-13 20:26:42 -0700

    5 block

--- Block Sliders/Assets/BlockSliders/Scripts/BallRoller.cs
+++ Block Sliders/Assets/BlockSliders/Scripts/BallRoller.cs
@@ -3,40 +3,39 @@ using System.Collections;
 
 public class BallRoller : MonoBehaviour {
 
-	public float speed;
 	private Vector3 acceleration;
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
 		acceleration = Vector3.zero;
-		
+
 		//up
 		if (Input.GetKey("w"))
 		{
-			acceleration.x += 1 * speed;
+			acceleration.x += 1;
 		}
 		
 		//down
 		if (Input.GetKey("s"))
 		{
-			acceleration.x -= 1 * speed;
+			acceleration.x -= 1;
 		}
 		
 		//left
 		if (Input.GetKey("a"))
 		{
-			acceleration.z += 1 * speed;
+			acceleration.z += 1;
 		}
 
 		//right
 		if (Input.GetKey("d"))
 		{
-			acceleration.z -= 1 * speed;
+			acceleration.z -= 1;
 		}
 		
-		//acceleration.Normalize();
-		acceleration *= speed;
+		acceleration.Normalize();
+		//acceleration *= speed;
 		rigidbody.AddTorque(acceleration);
 	}
 }
