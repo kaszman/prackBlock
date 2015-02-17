034e93530d9ed93042752f69a52572dc5202359f
kaszman <user@ChuckNorrisFanClub.local>
2015-02-17 16:08:20 -0700

    Main Menu 1

--- Block Sliders/Assets/BlockSliders/Scripts/QuitGame.cs
+++ Block Sliders/Assets/BlockSliders/Scripts/QuitGame.cs
@@ -2,11 +2,26 @@
 using System.Collections;
 
 public class QuitGame : MonoBehaviour {	
+
+	public void quitGame(bool AreYouSure)
+	{
+		if (AreYouSure = true)
+		{
+			Application.Quit();
+		}
+	}
+
+
+
+
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.Escape))
 	   {
-			Application.Quit();
+			quitGame(true);
 		}
 	}
+
+
 }
