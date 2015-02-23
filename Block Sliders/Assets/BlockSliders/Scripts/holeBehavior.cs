406f83c738029baec4c8c298ea128d895020f22e
kaszman <user@ChuckNorrisFanClub.local>
2015-02-23 16:41:52 -0700

    Lvl2 hole script inprogress
    Changes: trap hole script in the works
    
    Contents
    Scenes: Main Menu, Help menu, Lvl1, Lvl2

--- Block Sliders/Assets/BlockSliders/Scripts/holeBehavior.cs
+++ Block Sliders/Assets/BlockSliders/Scripts/holeBehavior.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class holeBehavior : MonoBehaviour {
 
+	public object ownerObject;
+	public SpriteRenderer ownerRenderer;	
+	public Sprite filledHole;
+	private GameObject collided;
+	private bool filled;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		filled = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+//what to do while the hole is empty
+		if (filled == false)
+		{
+		//if the object is a moveable block
+			if (collided.tag == "Enemy")
+			{	
+				//destroy the offencing object
+				Destroy(collided);
+
+				//set filled to true
+				filled = true;
+			}
+
+			//if the object is a player, (restart? move back before hole?)
+			else if (collided.tag == "Player")
+			{
+				//decide what to do
+			}
+		}
+
+//what to do if the hole is filled
+		else
+		{
+	//set the texture to the filled hole texture
+			ownerRenderer.sprite = filledHole;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) 
+	{			
+		collided = new GameObject();
 	}
 }
