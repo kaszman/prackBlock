c1bcac33988cf8162f801812a6632ef3f497cb82
kaszman <user@audrianna.uccs.edu>
2015-03-11 16:00:52 -0600

    canvas revamp

--- Block Sliders/Assets/BlockSliders/Scripts/MainMenu.cs
+++ Block Sliders/Assets/BlockSliders/Scripts/MainMenu.cs
@@ -37,7 +37,7 @@ public class MainMenu : MonoBehaviour {
 
 	public void changeCanvas(string canvas)
 	{			
-		//displayCanvas.enabled = false;
+		displayCanvas.enabled = false;
 		if (canvas == "main")
 		{
 			blockspeedSlider.enabled = false;
