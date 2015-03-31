760616f23d03214dfffca0969ff4bef367f1f3f4
kaszman <user@ChuckNorrisFanClub-2.local>
2015-03-31 16:57:18 -0600

    Level Selection progress
    needs some work, but basic layout and code is implemented

---
+++ Block Sliders/Assets/BlockSliders/Scripts/SceneSelect.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SceneSelect : MonoBehaviour
+{
+
+		//ui elements
+		public Text levelNameText;
+		public Button prevButton;
+		public Button nextButton;
+		public Button back;
+		public Button Select;
+		public Image levelPicture;
+
+		//int to select the level
+		private int selectionNumber = 1;
+
+
+		// Use this for initialization
+		void Start ()
+		{
+				GameControl.control.Load ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				levelPicture = RetrieveImage (selectionNumber);
+		}
+
+		//increment level selector, limited to unlocked levels
+		public void nextLevel ()
+		{
+				if (selectionNumber + 1 <= GameControl.control.HighestUnlock) {
+						selectionNumber++;
+				}
+		}
+
+		//decriment level selector, not below 1
+		public void backLevel ()
+		{
+				if (selectionNumber - 1 >= 1) {
+						selectionNumber--;
+				}
+		}
+
+		//load the selected level
+		public void SelectLevel ()
+		{
+				Application.LoadLevel ("Lvl" + selectionNumber.ToString ());
+		}
+
+
+		//retrieve the appropriate level image
+		private Image RetrieveImage (int lvl)
+		{
+
+		}
+}
