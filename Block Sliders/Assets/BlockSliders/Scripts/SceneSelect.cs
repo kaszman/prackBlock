fb0dfaf65578620b78b5a392e5738f8aa09e4a3b
kaszman <user@ChuckNorrisFanClub.local>
2015-04-15 21:11:41 -0600

    replaced some lvl images
    need to replace images for levels 2 - 12

--- Block Sliders/Assets/BlockSliders/Scripts/SceneSelect.cs
+++ Block Sliders/Assets/BlockSliders/Scripts/SceneSelect.cs
@@ -16,7 +16,7 @@ public class SceneSelect : MonoBehaviour
 		public Button Select;
 		public Image levelPicture;
 		public int numOfLevels;
-		public Sprite[] levelImages = new Sprite[18];
+		public Sprite[] levelImages = new Sprite[19];
 
 		//int to select the level
 		private int selectionNumber = 1;
